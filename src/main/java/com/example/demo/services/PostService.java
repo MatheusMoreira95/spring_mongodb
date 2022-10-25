7b017b7cc46a5b52f0d1ad92abec92c2b3bf91d7
Matheus Moreira <matheusjosemoreira@gmail.com>
2022-10-25 17:51:27 -0300

    post por id

---
+++ src/main/java/com/example/demo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.example.demo.services;
+
+import com.example.demo.domain.Post;
+import com.example.demo.repositories.PostRepository;
+import com.example.demo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
