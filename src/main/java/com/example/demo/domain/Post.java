26711588eac395a9406107dc283451a24c0805ce
Matheus Moreira <matheusjosemoreira@gmail.com>
2022-10-26 10:13:13 -0300

    acrescentando comentarios

--- src/main/java/com/example/demo/domain/Post.java
+++ src/main/java/com/example/demo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.example.demo.domain;
 
 import com.example.demo.dto.AuthorDTO;
+import com.example.demo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Document
 public class Post  implements Serializable {
@@ -16,6 +19,9 @@ public class Post  implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
+
 
 
     public Post() {
@@ -67,5 +73,12 @@ public class Post  implements Serializable {
 
     public void setBody(String body) {
         this.body = body;
+    }
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 }
