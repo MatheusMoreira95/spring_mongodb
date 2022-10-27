1d12cc3c2739bb0e93500db208437402a3f45244
Matheus Moreira <matheusjosemoreira@gmail.com>
2022-10-27 17:22:45 -0300

    consulta com varios criterios

--- src/main/java/com/example/demo/services/PostService.java
+++ src/main/java/com/example/demo/services/PostService.java
@@ -6,8 +6,10 @@ import com.example.demo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
 
 @Service
 public class PostService {
@@ -15,14 +17,16 @@ public class PostService {
     @Autowired
     private PostRepository repo;
 
-
     public Post findById(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
-    public List<Post> findByTitle(String text){
-      return  repo.searchTitle(text);
+    public List<Post> findByTitle(String text) {
+        return repo.searchTitle(text);
     }
 
+    public List<Post> fullSearch(String text) {
+        return repo.fullSearch(text);
+    }
 }
